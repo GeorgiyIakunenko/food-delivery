a4acfd652205c72a1ea152ddfb33c9095aa57a29
Georgiy <goshanyakunenko@gmail.com>
2023-06-28 19:00:17 +0200

    change main

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	_ "github.com/lib/pq"
 )
 
@@ -18,33 +17,6 @@ func main() {
 	err = db.Ping()
 	if err != nil {
 		panic(err)
-	}
-
-	var results []sql.NullString
-
-	userRow, err := db.Query("SELECT first_name FROM customer")
-	if err == sql.ErrNoRows {
-		// Check for empty result set
-		fmt.Println("NO ROWS !!!")
-	} else if err != nil {
-		panic(err)
-	}
-
-	for userRow.Next() {
-		username := sql.NullString{}
-		err = userRow.Scan(&username)
-		if err != nil {
-			panic(err)
-		}
-
-		results = append(results, username)
-	}
-
-	for _, name := range results {
-		v, _ := name.Value()
-		fmt.Println("name.Value() - ", v)
-		fmt.Println("name.Valid   - ", name.Valid)
-		fmt.Println("---")
 	}
 
 }
