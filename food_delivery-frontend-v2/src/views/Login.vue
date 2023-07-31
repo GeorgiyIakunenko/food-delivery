6b6818ee6a5cd1555c1476d8c99e936465b281bf
Georgiy Iakunenko <goshan3097@gmail.com>
2023-07-31 19:51:26 +0200

    added reusable input component

--- food_delivery-frontend-v2/src/views/Login.vue
+++ food_delivery-frontend-v2/src/views/Login.vue
@@ -1,4 +1,11 @@
 <script setup>
+import Input from "@/components/Input.vue";
+import {ref} from "vue";
+
+const loginForm = ref({
+  email: "",
+  password: ""
+});
 
 </script>
 
@@ -6,6 +13,11 @@
   <main>
     <div class="container">
       Login
+      <div class="form">
+        <Input v-model:value="loginForm.email"  label="Email Address" type="text" value="email" ></Input>
+        <Input v-model:value="loginForm.password"  label="Password" type="password" value="password" ></Input>
+      </div>
+
     </div>
   </main>
 </template>
