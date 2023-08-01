0c546f275cab08e3c50711a0eb94e58b9ec08314
Georgiy Iakunenko <goshan3097@gmail.com>
2023-08-01 15:07:11 +0200

    added tailwind prettier

--- food_delivery-frontend-v2/src/App.vue
+++ food_delivery-frontend-v2/src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
-import { ref } from 'vue'
-import Header from './components/Header.vue'
+import Header from "./components/Header.vue";
 </script>
 
 <template>
   <Header />
   <router-view></router-view>
 </template>
-
--- food_delivery-frontend-v2/src/components/Button.vue
+++ food_delivery-frontend-v2/src/components/Button.vue
@@ -1,39 +1,40 @@
 <script setup>
-  import {computed} from "vue";
+import { computed } from "vue";
 
-  const props = defineProps({
-    type: {
-      type: String,
-      validator(value) {
-        return ['standard', 'primary', 'secondary'].includes(value)
-      },
-      default: 'standard'
+const props = defineProps({
+  type: {
+    type: String,
+    validator(value) {
+      return ["standard", "primary", "secondary"].includes(value);
     },
-    disabled: {
-      type: Boolean,
-      default: true
-    }
-  })
+    default: "standard",
+  },
+  disabled: {
+    type: Boolean,
+    default: true,
+  },
+});
 
-  const buttonClass = computed(() => {
-    switch (props.type) {
-      case 'primary':
-        return !props.disabled
-            ? 'bg-primary-400 border-neutral-0 border-2 h-12 hover:bg-blue-75 hover:text-black active:transform-cpu font-medium transition duration-300 ease-in-out text-white'
-            : 'bg-primary-disabled border-neutral-0 border-2 h-12 hover:text-black active:transform-cpu font-medium transition duration-300 ease-in-out text-white'
-      case 'secondary':
-        return 'bg-secondary-btn hover:bg-blue-75 hover:text-black active:transform-cpu transition duration-300 ease-in-out'
-      default:
-        return 'bg-neutral-30 hover:bg-neutral-40 active:transform-cpu transition duration-300 ease-in-out border-0'
-    }
-  })
-
+const buttonClass = computed(() => {
+  switch (props.type) {
+    case "primary":
+      return !props.disabled
+        ? "bg-primary-400 border-neutral-0 border-2 h-12 hover:bg-blue-75 hover:text-black active:transform-cpu font-medium transition duration-300 ease-in-out text-white"
+        : "bg-primary-disabled border-neutral-0 border-2 h-12 hover:text-black active:transform-cpu font-medium transition duration-300 ease-in-out text-white";
+    case "secondary":
+      return "bg-secondary-btn hover:bg-blue-75 hover:text-black active:transform-cpu transition duration-300 ease-in-out";
+    default:
+      return "bg-neutral-30 hover:bg-neutral-40 active:transform-cpu transition duration-300 ease-in-out border-0";
+  }
+});
 </script>
 <template>
-  <button class="px-3.5 py-1.5 font-sans rounded-xl flex items-center justify-center" :class="buttonClass">
+  <button
+    class="px-3.5 py-1.5 font-sans rounded-xl flex items-center justify-center"
+    :class="buttonClass"
+  >
     <slot></slot>
   </button>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
