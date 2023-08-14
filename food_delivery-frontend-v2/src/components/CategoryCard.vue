e4536a843595d30e659aae9e77ab72a6e4909693
Georgiy Iakunenko <goshan3097@gmail.com>
2023-08-14 13:36:18 +0200

    category card fixed

--- food_delivery-frontend-v2/src/components/CategoryCard.vue
+++ food_delivery-frontend-v2/src/components/CategoryCard.vue
@@ -15,18 +15,20 @@ console.log(props);
     class="flex cursor-pointer flex-col items-center justify-center rounded-xl bg-amber-50 p-3 shadow-md transition-all duration-300 hover:scale-105"
   >
     <img
-      class="mb-2 w-36 md:w-52"
+      class="mb-2 h-36 w-36 md:h-52 md:w-52"
       :src="getImageUrl(category.image)"
       :alt="category.name"
     />
     <div class="px-1">
       <div class="flex items-center justify-center gap-3">
-        <div class="mb-1 font-sans text-lg font-medium">
+        <div class="mb-1 font-sans text-lg font-bold">
           {{ category.name }}
         </div>
       </div>
-      <div class="flex w-48 items-center text-center text-sm md:text-base">
-        {{ category.description.slice(0, 50) }}...
+      <div
+        class="flex w-40 items-center text-center text-sm md:w-48 md:text-base"
+      >
+        {{ category.description.slice(0, 60) }}...
       </div>
     </div>
   </div>
