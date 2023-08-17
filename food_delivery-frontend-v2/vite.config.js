0224fad29eaa8da0250b6500d1de98dfc5ef9667
Georgiy Iakunenko <goshan3097@gmail.com>
2023-08-17 14:30:51 +0200

    compressed images added

--- food_delivery-frontend-v2/vite.config.js
+++ food_delivery-frontend-v2/vite.config.js
@@ -1,20 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
-        script: {
-          defineModel : true,
-        }
-    })
+      script: {
+        defineModel: true,
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
